aa6b97914b8bcaf1fe1adcfa2b0d78f258711525
Gurleen Saini <gurleen22.27.22@gmail.com>
2025-04-08 12:00:26 +0530

    Update insertSort.cpp

--- insertSort.cpp
+++ insertSort.cpp
@@ -1,12 +1,10 @@
 #include <stdio.h>
-
 void insertionSort(int arr[], int n) {
     int i, key, j;
     for (i = 1; i < n; i++) {
         key = arr[i];
         j = i - 1;
 
-        // Move elements that are greater than key
         while (j >= 0 && arr[j] > key) {
             arr[j + 1] = arr[j];
             j--;
@@ -14,16 +12,14 @@ void insertionSort(int arr[], int n) {
         arr[j + 1] = key;
     }
 }
-
 void printArray(int arr[], int n) {
     for (int i = 0; i < n; i++)
         printf("%d ", arr[i]);
     printf("\n");
 }
-
 int main() {
     int arr[100], n;
-
+    
     printf("Enter number of elements: ");
     scanf("%d", &n);
 
@@ -38,7 +34,7 @@ int main() {
 
     printf("Sorted array:\n");
     printArray(arr, n);
-
+    
     return 0;
 }
 
